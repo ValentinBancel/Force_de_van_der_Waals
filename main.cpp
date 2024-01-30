477e36cf0400c6e662489658511f52f2359667fc
valentinbancel <valentin.bancel@orange.fr>
2024-01-30 15:22:14 +0100

    add(main): add main code who call data and send it into calcul methods

--- main.cpp
+++ main.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <string>
+#include <any>
+
 #include "data.hpp"
+#include "van_der_waals_lib.hpp"
+
+using namespace std;
 
 int main() {
-    int intValue = 42;
-    std::string stringValue = "Hello, C++!";
-
-    // Using the template function from data.hpp
-    bool isString = checkType(stringValue, typeid(std::string).name());
-    bool isInt = checkType(intValue, typeid(int).name());
-
-    std::cout << "Is a string: " << std::boolalpha << isString << std::endl;
-    std::cout << "Is an int: " << std::boolalpha << isInt << std::endl;
-
+    cout << "Start the van der waals program" << endl;
+    map<string, float> final_result;
+    map<string, float> data = data_entree();
+    final_result = van_der_waal(data);
+    cout << "The result is : " << final_result["Van_der_whaal"] << endl;
+    cout << "End of the van der waals program" << endl;
     return 0;
 }

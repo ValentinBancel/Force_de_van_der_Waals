25b33fef1a217b185ad65bd96589d491605312b2
valentinbancel <valentin.bancel@orange.fr>
2024-01-30 15:21:29 +0100

    add(calcul): cerate header file for calcul of van der waals weight

--- van_der_waals_lib.hpp
+++ van_der_waals_lib.hpp
@@ -4,22 +4,14 @@
 
 #ifndef MATH_HPP_INCLUDED
 #define MATH_HPP_INCLUDED
-class Van_der_Waals{
-    public:
-    
-    const float constante_dielectrique_vide;
-    const float constante_de_plank;
-    const float constante_de_boltzmann;
-    const float ratio_celsius_kelvin;
-    std::map<std::string, float> Van_der_whaal(float r, float T, float mu1, float mu2, float epsilon_r, float alpha1, float alpha2, float nu1, float nu2);
-    
-    private:
+// #pragma once
 
-    float Van_der_waals_weigth(float Keessom, float Debye, float London, float r);
-    float Keessom_weigth(float mu1, float mu2, float epsilon_r, float T);
-    float Debye_weigth(float epsilon_r, float alpha1, float alpha2, float mu1, float mu2);
-    float London_weigth(float epsilon_r, float alpha1, float alpha2, float nu1, float nu2);
+std::map<std::string, float> van_der_waal(std::map<std::string, float> data);
 
-};
 
-#endif // MATH_HPP_INCLUDED
+// float resultat;
+float Van_der_waals_weigth(float Keessom, float Debye, float London, float r);
+float Keessom_weigth(float mu1, float mu2, float epsilon_r, float T);
+float Debye_weigth(float epsilon_r, float alpha1, float alpha2, float mu1, float mu2);
+float London_weigth(float epsilon_r, float alpha1, float alpha2, float nu1, float nu2);
+#endif
